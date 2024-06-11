085215a4648af0a7d4f455f6c62c0cdbf9444fc9
Carlos Lima <Carllosaugusto@outlook.com>
2024-06-11 20:59:46 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,7 +6,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
+
+profile = webdriver.FirefoxProfile()
+profile.set_preference('browser.download.folderList', 2)
+profile.set_preference('browser.download.manager.showWhenStarting', False)
+profile.set_preference('browser.helperApps.neverAsk.saveToDisk', '')
+profile.set_preference('browser.download.manager.showAlertOnComplete', False)
+profile.set_preference('browser.download.manager.useWindow', False)
+profile.set_preference('browser.download.manager.focusWhenStarting', False)
+profile.set_preference('browser.download.manager.showWhenStarting', False)
+profile.set_preference('browser.download.manager.closeWhenDone', True)
 
 # Instantiate options
 options = Options()
@@ -25,15 +37,17 @@ service =  Service()
 
 # webdriver.ChromeOptions é usado para definir a preferencia para o navegador do Chrome
 # Esta ignorando as configurações que fiz no inicio
-options = webdriver.ChromeOptions()
+#options = webdriver.ChromeOptions()
 
 #Inicia-se a instancia do Chrome WebDrive com as definiçoes do option e service.
 #driver =  webdriver.Chrome(service=service, options=options)
 #driver = webdriver.Chrome(executable_path='/path/to/driver/chromedriver')
 #driver = webdriver.Chrome(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 url = 'https://www.ibge.gov.br/estatisticas/downloads-estatisticas.html'
+driver = webdriver.Firefox(
+    service=FirefoxService(GeckoDriverManager().install()))
 driver.get(url)
 print(driver.title)
 
