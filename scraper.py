7211684f590c99a4f999e2abccec9e20614f9172
Carlos Lima <Carllosaugusto@outlook.com>
2024-06-11 21:15:09 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,11 +8,13 @@ options.add_argument('-headless')  # Execute o Firefox em modo headless
 service = Service(executable_path='/usr/local/bin/geckodriver')
 driver = webdriver.Firefox(service=service, options=options)
 
-driver.get('http://github.com')
+# Seu código para usar o driver vai aqui
+url = 'https://www.ibge.gov.br/estatisticas/downloads-estatisticas.html'
+driver.get(url)
 print(driver.title)
 with open('./GitHub_Action_Results.txt', 'w') as f:
     f.write(f"This was written with a GitHub action {driver.title}")
 
-# Seu código para usar o driver vai aqui
+
 
 driver.quit()
