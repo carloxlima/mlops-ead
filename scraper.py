2afe3ea178098c3a82191d43eb24a23e9b3e05a8
Carlos Lima <Carllosaugusto@outlook.com>
2024-06-11 21:11:51 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,6 +8,11 @@ options.add_argument('-headless')  # Execute o Firefox em modo headless
 service = Service(executable_path='/usr/local/bin/geckodriver')
 driver = webdriver.Firefox(service=service, options=options)
 
+driver.get('http://github.com')
+print(driver.title)
+with open('./GitHub_Action_Results.txt', 'w') as f:
+    f.write(f"This was written with a GitHub action {driver.title}")
+
 # Seu código para usar o driver vai aqui
 
 driver.quit()
