716b16e1d5b5ee091d5fa27142fda76fc89c4a61
Carlos Lima <Carllosaugusto@outlook.com>
2024-06-24 17:19:09 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -16,6 +16,13 @@ print(car_data)
 options = Options()
 options.add_argument('-headless')  # Execute o Firefox em modo headless
 
+options.set_preference("browser.download.folderList", 2)
+options.set_preference("browser.download.dir", download_path)
+options.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv")
+options.set_preference("browser.download.manager.showWhenStarting", False)
+# Desativa o visualizador de PDF embutido
+options.set_preference("pdfjs.disabled", True)
+
 service = Service(executable_path='/usr/local/bin/geckodriver')
 driver = webdriver.Firefox(service=service, options=options)
 
